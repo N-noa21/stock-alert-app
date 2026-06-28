9e73bc1a308274344be53baed71e0800b7473b6f
N-noa21 <nnoa21.code@gmail.com>
2026-06-28 13:47:12 +0900

    Return auth token and support bearer auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -58,9 +58,9 @@ authRouter.post("/register", async (req, res) => {
 
 
   const token = signToken({ userId: user.id });
-
+  
   res.cookie("token", token, authCookieOptions);
-
+  
   return res.status(201).json({
     user,
     token,
